cc2ba5c58c325167683eee030497dda3c9110353
Naja <najariski17@gmail.com>
2026-07-20 23:07:32 +0700

    feat(sales): add sales model

---
+++ models/saleModel.js
@@ -0,0 +1,27 @@
+import db from "../config/db.js";
+
+const SalesModel = {
+
+    async getAll() {
+
+        const [rows] = await db.query(`
+            SELECT
+                sales.id,
+                customers.name AS customer,
+                sales.sale_date,
+                sales.total,
+                sales.payment_method,
+                sales.status
+            FROM sales
+            JOIN customers
+                ON sales.customer_id = customers.id
+            ORDER BY sales.id DESC
+        `);
+
+        return rows;
+
+    }
+
+};
+
+export default SalesModel;
